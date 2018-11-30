d832746b263be1b946f8bce2cb6ef693f9cadde0
sterry95 <dwaynewilson05@gmail.com>
2018-11-29 22:02:32 -0600

    SON PARA CONEXIONES A LOS USUARIOS  A LA BASE DE DATOS

---
+++ adminenlaces.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>COMPROMAR</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="COMPROMAR">
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+<body data-offset="40" background="images/logo.png" style="background-attachment: fixed">
+<div class="container">
+<header class="header">
+<div class="row">
+	<?php
+	include("include/cabecera.php");
+	?>
+</div>
+</header>
+
+<div class="navbar">
+  <div class="navbar-inner">
+	<div class="container">
+	  <div class="nav-collapse">
+		<ul class="nav">
+			<li class=""><a href="admin.php">ADMINISTRADOR DEL SITIO</a></li>
+			<li class=""><a href="adminenlaces.php">AGREGAR ENLACES</a></li>
+	
+		</ul>
+		<form action="#" class="navbar-search form-inline" style="margin-top:6px">
+		
+		</form>
+		<ul class="nav pull-right">
+			  <li><a href="index.php"> Cerrar Cesión </a></li>			 
+		</ul>
+	  </div><!-- /.nav-collapse -->
+	</div>
+  </div><!-- /navbar-inner -->
+</div>
+
+<!-- ======================================================================================================================== -->
+<div class="row">
+			
+	<div class="span12">
+
+		<div class="caption">
+
+				</div>
+			</div>
+					
+</div>
+
+	</div>
+</div>
+<!-- Footer
+      ================================================== -->
+<hr class="soften"/>
+<footer class="footer">
+
+<hr class="soften"/>
+<p>&copy; COMPRAMAR <br/><br/></p>
+ </footer>
+</div><!-- /container -->
+
+
+    <script src="bootstrap/js/jquery-1.8.3.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+	</style>
+  </body>
+</html>
